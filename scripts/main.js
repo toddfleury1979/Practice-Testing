562b8fb4921dab900bd2d348137eb40fd7e5d64a
Todd Fleury <33099680+toddfleury1979@users.noreply.github.com>
2017-11-01 16:20:13 -0400

    new

---
+++ scripts/main.js
@@ -0,0 +1,77 @@
+var detail_image_selector = '[data-image-role="target"]';
+var detail_title_selector = '[data-image-role="title"]';
+var detail_frame_selector = '[data-image-role="frame"]';
+var thumbnail_link_selector = '[data-image-role="trigger"]';
+var hidden_detail_class = 'hidden-detail';
+var tiny_effects_class = 'is-tiny';
+var ESC_KEY = 27;
+
+function setDetails(imageUrl, titleText) {
+  'use strict';
+  var detailImage = document.querySelector(detail_image_selector);
+  detailImage.setAttribute('src', imageUrl);
+
+  var detailImage = document.querySelector(detail_title_selector);
+  detailImage.textContent = titleText;
+}
+
+function imageFromThumb(thumbnail) {
+  'use strict';
+  return thumbnail.getAttribute('data-image-url');
+}
+
+function titleFromThumb(thumbnail) {
+  'use strict';
+  return thumbnail.getAttribute('data-image-title');
+}
+
+function setDetailsFromThumb(thumbnail) {
+  'use strict';
+  setDetails(imageFromThumb(thumbnail), titleFromThumb(thumbnail));
+}
+
+function addThumbClickHandler(thumb) {
+  'use strict';
+  thumb.addEventListener('click', function (event) {
+    event.preventDefault();
+    setDetailsFromThumb(thumb);
+    showDetails();
+  });
+}
+
+function getThumbnailsArray() {
+  'use strict';
+  var thumbnails = document.querySelectorAll(thumbnail_link_selector);
+  var thumbnailArray = [].slice.call(thumbnails);
+  return thumbnailArray;
+}
+
+function hideDetails() {
+  'use strict';
+  document.body.classList.add(hidden_detail_class);
+}
+
+function showDetails() {
+  'use strict'
+  document.body.classList.remove(hidden_detail_class);
+}
+
+function addKeyPressHandler() {
+  'use strict'
+  document.body.addEventListener('keyup', function (event) {
+    event.preventDefault();
+    console.log(event.keyCode);
+    if (event.keyCode === ESC_KEY) {
+      hideDetails();
+    }
+  });
+}
+
+function initializeEvents() {
+  'use strict';
+  var thumbnails = getThumbnailsArray();
+  thumbnails.forEach(addThumbClickHandler);
+  addKeyPressHandler();
+}
+
+initializeEvents();
